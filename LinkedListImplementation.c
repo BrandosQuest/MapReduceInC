a125d92cca317ca64f56c39209ed5c5aec895f79
Brando Calderara <151777266+BrandosQuest@users.noreply.github.com>
2025-12-13 18:02:33 +0100

    now the list is working

--- LinkedListImplementation.c
+++ LinkedListImplementation.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct Node Node;
 
 struct Node {
     void* contentPointer;
     Node* next;
-    const Node* prev;
-    uint32_t index;
+    Node* prev;
+    int32_t index;
 };
 
 
 Node* createList() {
-    return calloc(1, sizeof(Node));
+    Node* node = calloc(1, sizeof(Node));
+    if (node == NULL) {
+        perror("Failed to create list");
+        exit(EXIT_FAILURE);
+    }
+    node->index=-1;
+    return node;
 }
 
 //returns the new head from the previous tail, if only one node, returns the head from the tail
 Node* addNode(Node* head, void* contentPointer) {
     Node* nodeJustCreated = calloc(1, sizeof(Node));
+    if (nodeJustCreated == NULL) {
+        return NULL; // Allocation failed
+    }
     nodeJustCreated->contentPointer = contentPointer;
+    nodeJustCreated->index = head->index+1;
     head->next = nodeJustCreated;
     nodeJustCreated->prev = head;
     return nodeJustCreated;
@@ -34,25 +45,52 @@ void freeList(Node* tail) {
 }
 void printList(Node* tail) {
     Node* temp;
-    while (tail != NULL) {
+    tail= tail->next;
+    while (tail != NULL && tail->contentPointer != 0) {
         temp = tail->next;
-        printf("%s\n", (const char*)tail->contentPointer);
+        printf("Index: %d\tContentString: %s\n", tail->index,(const char*)tail->contentPointer);
         fflush(stdout);
         tail = temp;
     }
+}
+/*void printList(Node* head) {
+    Node* temp;
+    while (head != NULL) {
+        temp = head->prev;
+        printf("%s\n", (const char*)head->contentPointer);
+        fflush(stdout);
+        head = temp;
+    }
+}*/
+Node* getNode(Node* tail, int index) {
+    Node* temp;
+    tail= tail->next;
+    while (tail != NULL && tail->contentPointer != 0) {
+        temp = tail->next;
+        if (temp->index == index)
+        {
+            return temp;
+        }
+        tail = temp;
+    }
+    return NULL;
 }
 
 int main () {
     Node* tail = createList();
     printList(tail);
     Node* head = tail;
-    char buffer[35];
-
+    char buffer[32];
     for (int i = 0; i < 100; ++i)
     {
-        printf("%s", _itoa(i, buffer, 10));
-        head = addNode(head, _itoa(i, buffer, 10));
+        _itoa(i, buffer, 10);
+        head = addNode(head, strdup(buffer));
     }
     printList(tail);
+
+
+    //get the pointer to the 44th node
+    Node* getted = getNode(tail, 44);
+    printf("Getted from index 44\nIndex: %d\tContentString: %s\n", getted->index,(const char*)getted->contentPointer);
     freeList(tail);
 }
